34c4eb532aa64ccda57f6a7c6994e67eefa2ef7e
ivanfilho <ivanfcfilho@gmail.com>
2018-04-26 18:21:45 -0300

    Working

--- app.py
+++ app.py
@@ -21,6 +21,22 @@ def showSignUp():
 def ajaxSignup():
     return aj.signUp(request.form)
 
+@app.route('/ajax/login', methods=['POST'])
+def ajaxLogin():
+    r = aj.login(request.form)
+    if(r[1] == 200):
+        session['username'] = 'user'
+        session.modified = True
+    else:
+        print('Erro')
+    return r
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    session.modified = True
+    return render_template('index.html')
+
 if __name__ == "__main__":
     app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
     app.run(port=5000)
--- static/js/signUp.js
+++ static/js/signUp.js
@@ -5,10 +5,20 @@ $(function(){
             url: '/ajax/signup',
 			data: $('form').serialize(),
 			type: 'POST',
-			success: function(response){
-				console.log(response);
+			success: function(data, textStatus, xhr){
+                if(xhr.status == 200) {
+                    alert('Sucesso')
+                    window.location.href = "/login";
+                } else {
+                    res = JSON.parse(data)
+                    alert('Error'+res.message)
+                }
 			},
-			error: function(request, error){
+			error: function(data, request, error){
+                var res = ''
+                if(data != '')
+                    res = JSON.parse(data.responseText)
+                alert('Erro'+res.Message)
 				console.log(error);
 			}
 		});
